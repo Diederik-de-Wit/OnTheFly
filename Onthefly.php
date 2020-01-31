861a8599f90a8533f23ad3172afbc98d207096e7
Diederik de Wit <d276022@edu.rocwb.nl>
2020-01-31 11:52:40 +0100

    init

---
+++ Onthefly.php
@@ -0,0 +1,33 @@
+<!Doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="vliegtuig.css">
+    <title>On The Fly</title>
+</head>
+<body>
+
+<ul class="topnav">
+    <li><a class="active" href="blijdorp.php">Home</a></li>
+    <li><a href="blijdorpdierenverblijf.php">Vliegtuigen</a></li>
+    <li><a href="blijdorpdierenverblijf.php">Planning</a></li>
+
+</ul>
+
+<div class="bg"></div>
+<div class="bg bg2"></div>
+<div class="bg bg3"></div>
+<div class="content">
+    <h1>De Wit Airlines</h1>
+</div>
+
+
+<img src="Blijdorp.png" alt="logo">
+
+
+<footer id="main-footer">
+    <p>Copyright © 2020. Van Diederik de Wit.  </p>
+</footer>
+
+</body>
+</html>
